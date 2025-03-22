103e82bbd0e781b22ce59714fd1d2edf5987a0f5
jbrocart <jbrocart93@gmail.com>
2025-03-21 20:29:17 -0600

    added scripts

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+const initialCards = [
+  {name: "Stormy sky", link: "https://unsplash.com/photos/a-beach-hut-sitting-on-top-of-a-sandy-beach-5VaF7hzo4wc"},
+  {name: "Sunset over the ocean", link: "https://unsplash.com/photos/silhouette-of-stone-on-seashore-during-golden-hour-63JKK67yGUE"},
+  {name: "Canyon Arch", link: "https://unsplash.com/photos/grand-canyon-mountain-tNN6zkUnTgg"},
+  {name: "Beautiful Milky Way in the mountains", link: "https://unsplash.com/photos/pine-trees-during-nighttime-7pUHeP1GRC4"},
+  {name: "Red fish in coral", link: "https://unsplash.com/photos/red-fish-beside-pink-coral-EDfZ0Sjmp_w"},
+  {name: "Camping in the mountains uder the stars", link: "https://unsplash.com/photos/person-sitting-near-bonfire-surrounded-by-trees-1azAjl8FTnU"},
+];
+
+console.log(initialCards);
