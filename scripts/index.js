07c64fa9fc0b3a8ae27c71b9550d15ca73308863
jbrocart <jbrocart93@gmail.com>
2025-03-30 19:22:17 -0600

    implement modal to JS

--- scripts/index.js
+++ scripts/index.js
@@ -6,3 +6,21 @@ const initialCards = [
   {name: "Red fish in coral", link: "https://unsplash.com/photos/red-fish-beside-pink-coral-EDfZ0Sjmp_w"},
   {name: "Camping in the mountains uder the stars", link: "https://unsplash.com/photos/person-sitting-near-bonfire-surrounded-by-trees-1azAjl8FTnU"},
 ];
+
+const profileEditButton = document.querySelector('.profile__edit-btn');
+
+const editProfileModal = document.querySelector('#edit-profile-modal');
+
+const closeButton = document.querySelector('.modal__close-btn');
+
+function openModal() {
+  editProfileModal.classList.add('modal_opened');
+}
+
+function closeModal() {
+  editProfileModal.classList.remove('modal_opened');
+}
+
+profileEditButton.addEventListener('click', openModal);
+
+closeButton.addEventListener('click', closeModal);
