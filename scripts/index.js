57df9ec0e1494b81d2db5cfffa1a077cde5d6848
jbrocart <jbrocart93@gmail.com>
2025-04-03 23:11:21 -0600

    form submission

--- scripts/index.js
+++ scripts/index.js
@@ -7,11 +7,23 @@ const initialCards = [
   {name: "Camping in the mountains uder the stars", link: "https://unsplash.com/photos/person-sitting-near-bonfire-surrounded-by-trees-1azAjl8FTnU"},
 ];
 
-const profileEditButton = document.querySelector('.profile__edit-btn');
-
 const editProfileModal = document.querySelector('#edit-profile-modal');
-
+const profileNameElement = document.querySelector('.profile__name');
+const profileDescriptionElement = document.querySelector('.profile__description');
+const profileEditButton = document.querySelector('.profile__edit-btn');
+const profileFormElement = editProfileModal.querySelector('.modal__form');
+const editProfileNameInput = editProfileModal.querySelector('#profile-name-input');
+const editProfileDescriptionInput = editProfileModal.querySelector('#profile-description-input');
 const closeButton = document.querySelector('.modal__close-btn');
+const addCardFormElement = editProfileModal.querySelector('#addCardModal');
+const nameInput = editProfileModal.querySelector('.add_card');
+const linkInput = editProfileModal.querySelector('#card-url-input');
+const addCardModal = document.querySelector('#add-card-modal');
+const addPostButton = document.querySelector('.profile__add-btn');
+const cardTitleInput = document.querySelector('#cardTitleInput');
+
+editProfileNameInput.value = profileNameElement.textContent;
+editProfileDescriptionInput.value = profileDescriptionElement.textContent;
 
 function openModal() {
   editProfileModal.classList.add('modal_opened');
@@ -21,6 +33,30 @@ function closeModal() {
   editProfileModal.classList.remove('modal_opened');
 }
 
-profileEditButton.addEventListener('click', openModal);
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
 
+profileNameElement.textContent = editProfileNameInput.value;
+profileDescriptionElement.textContent = editProfileDescriptionInput.value;
+
+closeModal();
+}
+
+function openAddCardModal() {
+  addCardModal.classList.add('modal_opened');
+}
+
+function handleAddCardSubmit(evt) {
+  evt.preventDefault();
+
+  console.log(titleInput.value);
+  console.log(linkInput.value);
+
+  closeModal();
+}
+
+profileEditButton.addEventListener('click', openModal);
 closeButton.addEventListener('click', closeModal);
+addPostButton.addEventListener('click', openAddCardModal);
+addCardFormElement.addEventListener('submit', handleAddCardSubmit);
+profileFormElement.addEventListener('submit', handleProfileFormSubmit);
